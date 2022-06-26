87cfb60de29202a06665f574cfb4645135be48d7
TimurTsarev <54883810+TimurTsarev@users.noreply.github.com>
2022-06-26 19:32:03 +0300

    Update customproxymodel.h

--- HseQtProject/customproxymodel.h
+++ HseQtProject/customproxymodel.h
@@ -7,25 +7,38 @@ class CustomProxyModel : public QSortFilterProxyModel
 {
     Q_OBJECT
 public:
-    explicit CustomProxyModel(QObject *parent = nullptr, QString name = "", QString votes = "", QString rating = "", QString adress = "",
+    explicit CustomProxyModel(QObject *parent = nullptr, QString name = "", QString votes = "", QString rating = "",
+                              QString adress = "",
                               QString cuisine = "", QString openHours = "");
+
     void setNameOfRestaurant(QString name);
+
     void setNumberVotes(QString num);
+
     void setRating(QString rating);
+
     void setAdress(QString adress);
+
     void setCuisine(QString cuisine);
+
     void setOpenHours(QString openHours);
+
     bool nameIsCorrect(QString name) const;
+
     bool votesIsCorrect(QString votes) const;
+
     bool ratingIsCorrect(QString rating) const;
+
     bool adressIsCorrect(QString adress) const;
+
     bool cuisineIsCorrect(QString cuisine) const;
+
     bool openHoursIsCorrect(QString openHours) const;
 
 protected:
     bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override;
 
-signals:
+    signals:
 
 private:
     QString nameRestaurant;
