9ac21afde3046948550f6feda08ebe31e13d8074
TimurTsarev <54883810+TimurTsarev@users.noreply.github.com>
2022-06-26 19:33:21 +0300

    Update loaddatafile.h

--- HseQtProject/loaddatafile.h
+++ HseQtProject/loaddatafile.h
@@ -10,27 +10,33 @@ class LoadDataFile : public QAbstractTableModel
 public:
     explicit LoadDataFile(QObject *parent = nullptr);
 
-    void fillDataTableFromFile (QString path);
-    void saveDataTableToFile (QString path);
+    void fillDataTableFromFile(QString path);
+
+    void saveDataTableToFile(QString path);
 
     // Header:
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
     // Basic functionality:
     bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role) override;
+
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
     bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
     Qt::ItemFlags flags(const QModelIndex &index) const override;
 
-    void appendRow(const QList<QVariant> &row);
+    void appendRow(const QList <QVariant> &row);
+
     void removeRow(int idx);
 
 private:
-    QList<QString> headerList;
-    QList<QList<QVariant>> dataTable;
+    QList <QString> headerList;
+    QList <QList<QVariant>> dataTable;
 };
 
 #endif // LOADDATAFILE_H
